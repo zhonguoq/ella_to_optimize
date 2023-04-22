3c91adb350eb39e32ce9880ed962d80e4233d3fa
Guoqiang Zhong <zhonguoq@126.com>
2023-04-22 15:20:12 +0800

    minify repo

--- assets/password-modal.js
+++ assets/password-modal.js
@@ -1,9 +1,1 @@
-class PasswordModal extends DetailsModal {
-  constructor() {
-    super();
-
-    if (this.querySelector('input[aria-invalid="true"]')) this.open({target: this.querySelector('details')});
-  }
-}
-
-customElements.define('password-modal', PasswordModal);
+class PasswordModal extends DetailsModal{constructor(){super(),this.querySelector('input[aria-invalid="true"]')&&this.open({target:this.querySelector("details")})}}customElements.define("password-modal",PasswordModal);
